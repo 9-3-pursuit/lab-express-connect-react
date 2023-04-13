4b1e130262350097e26c852e2517b2eb20942ef5
amber-king <amberking@pursuit.org>
2023-04-13 19:58:48 -0400

    updated/modified app.js & all pages folder js files and adjusted components js files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
+// Dependencies
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Component
 import NavBar from "./Components/NavBar";
-import NewLogForm from "./Components/NewLogForm";
-import EditLogForm from "./Components/EditLogForm";
-import LogDetails from "./Components/LogDetails";
-import LogsIndex from ".?Componenets/LogsIndex";
+
+// Pages
+import Edit from "./Pages-Links/Edit";
+import FourOFour from "./Pages-Links/FourOFour";
 import Home from "./Pages-Links/Home";
+import Index from "./Pages/Index";
+import New from "./Pages-Links/New";
+import Show from "./Pages-Links/Show";
 
 function App() {
   return (
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/logs" element={<LogsIndex />}></Route>
-        <Route path="/logs/:index" element={<LogDetails />}></Route>
-        <Route path="/logs/new" element={<NewLogForm />}></Route>
-        <Route path="/logs/:index/edit" element={<EditLogForm />}></Route>
-      </Routes>
-    </Router>
+    <div className="App">
+      <Router>
+        <NavBar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/logs" element={<Index />}></Route>
+            <Route path="/logs/:index" element={<Show />}></Route>
+            <Route path="/logs/new" element={<New />}></Route>
+            <Route path="/logs/:index/edit" element={<Edit />}></Route>
+            <Route path="*" element={<FourOFour />}></Route>
+          </Routes>
+        </main>
+      </Router>
+    </div>
   );
 }
 
---
+++ src/Pages-Links/Home.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div className="Home">
+      <h2>Welcome to the Captain's Log 🏴‍☠️</h2>
+      <p> Need Info on a Captains' journey, Search Away! </p>
+    </div>
+  );
+}
+
+export default Home;
