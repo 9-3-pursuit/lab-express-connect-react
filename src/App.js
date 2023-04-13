fc23026ecb334dcce6e1e39112ea0c70403d7427
amber-king <amberking@pursuit.org>
2023-04-13 19:31:49 -0400

    routes added to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import {} from ""
-function App() {
-// const URL = process.env.REACT_APP_API_URL 
-// console.log(URL)
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./Components/NavBar";
+import NewLogForm from "./Components/NewLogForm";
+import EditLogForm from "./Components/EditLogForm";
+import LogDetails from "./Components/LogDetails";
+import LogsIndex from ".?Componenets/LogsIndex";
+import Home from "./Pages-Links/Home";
 
-  return <div>Hello World</div>;
+function App() {
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/logs" element={<LogsIndex />}></Route>
+        <Route path="/logs/:index" element={<LogDetails />}></Route>
+        <Route path="/logs/new" element={<NewLogForm />}></Route>
+        <Route path="/logs/:index/edit" element={<EditLogForm />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
