76e41fd5f47fd7a2767547e3523e6c51ae201a55
joshuanelsondev <joshuanelsondev@gmail.com>
2023-04-14 15:26:01 -0400

    Add Pages folder its components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div className="App"> 
+      <Router>
+        <NavBar />
+        <main>
+          <Routes>
+            <Route path="/" element={} />
+            <Route path="/logs" element={} />
+            <Route path="/logs" element={} />
+            <Route path="/logs/:index" element={} />
+            <Route path="/logs/:index/edit" element={} />
+            <Route path="*" element={} />
+
+          </Routes>
+        </main>
+      </Router>
+    </div>
+  ) 
 }
 
 export default App;
