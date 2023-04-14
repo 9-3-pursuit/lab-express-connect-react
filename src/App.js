dd5d752e4a026eaec5686ccb37404576a50be2fc
joshuanelsondev <joshuanelsondev@gmail.com>
2023-04-14 15:39:07 -0400

    Add Pages and Routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
+// DEPENDENCIES
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// PAGES
+import Edit from "./Pages/Edit";
+import FourOFour from "./Pages/FourOFour";
+import Home from "./Pages/Home";
+import Index from "./Pages/Index";
+import New from "./Pages/New";
+import Show from "./Pages/Show";
 
 function App() {
   return (
@@ -7,12 +16,12 @@ function App() {
         <NavBar />
         <main>
           <Routes>
-            <Route path="/" element={} />
-            <Route path="/logs" element={} />
-            <Route path="/logs" element={} />
-            <Route path="/logs/:index" element={} />
-            <Route path="/logs/:index/edit" element={} />
-            <Route path="*" element={} />
+            <Route path="/" element={<Home />} />
+            <Route path="/logs" element={<Index />} />
+            <Route path="/logs" element={<New />} />
+            <Route path="/logs/:index" element={<Show />} />
+            <Route path="/logs/:index/edit" element={<Edit />} />
+            <Route path="*" element={<FourOFour />} />
 
           </Routes>
         </main>
