4d77e972d32fd201f50a4edaf4618a8eacf9fa87
joshuanelsondev <joshuanelsondev@gmail.com>
2023-04-14 17:22:40 -0400

    Edit Log.js

---
+++ src/Components/Logs.js
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Log from "./Log";
+
+export default function Logs() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -1,3 +1,10 @@
+import Logs from "../Components/Logs";
+
 export default function Index() {
-    
+    return (
+        <div>
+            <h2 className="text-4xl pt-4 pl-12">Index</h2>
+            <Logs />
+        </div>
+    )
 }
