db05ba473c81bab952148ccb5ba9ac6cb13e05a5
amber-king <amberking@pursuit.org>
2023-04-14 13:23:29 -0400

    log.js code added

--- src/Components/Log.js
+++ src/Components/Log.js
@@ -1,3 +1,23 @@
-export default function Log() {
-    return null;
+import { Link } from "react-router-dom";
+
+export default function Log({log, index}) {
+    return (
+        <tr className="Log">
+          <td>
+            {log.mistakesWereMadeToday ? (
+              <span>🤯</span>
+            ) : (
+              <span>&nbsp; &nbsp; &nbsp;</span>
+            )}
+          </td>
+          <td>
+            <a href={log.url} target="_blank" rel="noreferrer">
+              {log.name}
+            </a>
+          </td>
+          <td>
+            <Link to={`/logs/${index}`}>{log.title}</Link>
+          </td>
+        </tr>
+      );;
 }
