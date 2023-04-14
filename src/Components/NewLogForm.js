5aa5b8cd66ad77b86ab94afe7fcf4b6afa3a3f00
amber-king <amberking@pursuit.org>
2023-04-14 14:32:28 -0400

    added new log form js code

--- src/Components/NewLogForm.js
+++ src/Components/NewLogForm.js
@@ -1,3 +1,83 @@
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 export default function NewLogForm() {
-    return null;
+  const [log, setLog] = useState({
+    captainName: "",
+    title: "",
+    post: "",
+    mistakesWereMadeToday: false,
+    daysSinceLastCrisis: "",
+  });
+
+  const navigate = useNavigate();
+  const handleTextChange = (event) => {
+    setLog({ ...log, [event.target.id]: event.target.value });
+  };
+
+  const handleCheckboxChange = () => {
+    setLog({ ...log, mistakesWereMadeToday: !log.mistakesWereMadeToday });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post(`${process.env.REACT_APP_API_URL}/logs`, log)
+      .then(() => {
+        navigate("/logs");
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+  return (
+    <div className="New">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="captainName">Captain's Name:</label>
+        <input
+          id="captainName"
+          value={log.captainName}
+          type="text"
+          onChange={handleTextChange}
+          placeholder="Name of Captain"
+          required
+        />
+        <label htmlFor="title">Title:</label>
+        <input
+          id="title"
+          type="text"
+          value={log.title}
+          onChange={handleTextChange}
+        />
+        <label htmlFor="post">Post:</label>
+        <input
+          id="post"
+          type="text"
+          value={log.post}
+          onChange={handleTextChange}
+        />
+
+        <label htmlFor="daysSinceLastCrisis">Days Since Last Crisis:</label>
+        <textarea
+          id=" daysSinceLastCrisis"
+          value={log.daysSinceLastCrisis}
+          onChange={handleTextChange}
+          placeholder="ex:0"
+        />
+
+        <label htmlFor="mistakesWereMadeToday">Mistakes Were Made Today:</label>
+        <input
+          id=" mistakesWereMadeToday"
+          type="checkbox"
+          onChange={handleCheckboxChange}
+          checked={log.mistakesWereMadeToday}
+        />
+
+        <br />
+        <input type="submit" />
+      </form>
+     
+    </div>
+  );
 }
