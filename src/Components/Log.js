c2b01286e3c7f325afa944cb74460b2c31797e23
joshuanelsondev <joshuanelsondev@gmail.com>
2023-04-15 19:33:21 -0400

    Edit Logs.js and Log.js

--- src/Components/Log.js
+++ src/Components/Log.js
@@ -1,7 +1,12 @@
-export default function Log() {
-    return (
-        <div>
-            
-        </div>
-    )
+import { FcCheckmark } from "react-icons/fc";
+
+
+export default function Log({ log, index }) {
+  return (
+    <div className="grid grid-cols-3 justify-items-center">
+      {log.mistakesWereMadeToday && <FcCheckmark />}
+      <li>{log.captainName}</li>
+      <li>{log.title}</li>
+    </div>
+  );
 }
--- src/Components/Logs.js
+++ src/Components/Logs.js
@@ -3,9 +3,27 @@ import axios from "axios";
 import Log from "./Log";
 
 export default function Logs() {
+    const [logs, setLogs] = useState([]);
+
+    useEffect(() => {
+        axios.get((`${process.env.REACT_APP_API_URL}/logs`))
+            .then((res) => {
+                setLogs(res.data)
+            }).catch((e) => {
+                console.log(e);
+            })
+    }, [])
+
     return (
-        <div>
-            
+      <div className="bg-[#EDFAFC] mx-40 rounded-xl">
+        <div className="grid grid-cols-3 justify-items-center">
+          <h2 className="font-bold">Mistakes</h2>
+          <h2 className="font-bold">Captain Name</h2>
+          <h2 className="font-bold">See this log</h2>
         </div>
-    )
+        {logs.map((log, index) => {
+          return <Log key={index} log={log} index={index} />;
+        })}
+      </div>
+    );
 }
