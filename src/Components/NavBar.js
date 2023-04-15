401c954b8a595c7d78c554fe71899b1421d5d9bd
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-04-15 18:03:45 -0400

    Updates navbar for tests

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <nav className="w-full px-4 h-16 bg-blue-700 shadow-md flex items-center justify-between">
-      <Link to="/">
-        <h1 className="text-3xl text-white">Captains Log</h1>
+    <nav className="w-full px-4 h-16 bg-blue-700 shadow-md flex items-center justify-between fixed">
+      <Link to="/logs">
+        <h1 className="text-3xl text-white">Captain's Log</h1>
       </Link>
       <button className="text-white rounded-md p-2 border-white border hover:bg-white hover:text-blue-700 hover:cursor-pointer">
-        New Log
+        <Link to="/logs/new">New Log</Link>
       </button>
     </nav>
   );
