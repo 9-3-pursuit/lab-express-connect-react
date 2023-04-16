cfc4e572634e785ceba0f2330aecc88107880fc5
Marangely Rosas <Ladykaliber22@gmail.com>
2023-04-16 16:03:08 -0400

    Created NavBar.css for styling of NavBar.js, Linked All Logs and New Log in NavBar.js

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,14 +1,26 @@
 import { Link } from "react-router-dom";
+import "./NavBar.css"
 
 export default function NavBar() {
   return (
-    <nav>
-      <h1>
-        <Link to="/logs">Logs</Link>
-      </h1>
-      <button>
-        <Link to="/logs/new">New Log</Link>
-      </button>
-    </nav>
+    <header>
+      <article>
+        <h1>
+          <Link to="/">
+            Captain's Logs
+          </Link>
+        </h1>
+      </article>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/logs">All Logs</Link>
+          </li>
+          <li>
+            <Link to="/logs/new">New Log</Link>
+          </li>
+        </ul>   
+      </nav>
+    </header>
   );
 }
