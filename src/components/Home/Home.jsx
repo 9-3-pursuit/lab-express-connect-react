337b978356c5025770113d227ac093c134276fc2
HaroldF415 <harold.f415@gmail.com>
2023-04-17 03:23:52 -0400

    separating concerns with the API calls

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Captain's Log!</h1>
+      <p>Use this app to view previous logs or add a new one.</p>
+    </div>
+  );
+};
+
+export default Home;
