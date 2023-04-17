bc62d608dddf6ef7ca07c26e2c0f3253879bacfe
HaroldF415 <harold.f415@gmail.com>
2023-04-17 03:11:01 -0400

    importing NavBar and react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
-function App() {
-  return <div>Hello World</div>;
-}
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { NavBar } from "./components";
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
 
 export default App;
