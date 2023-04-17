618a7303f08fc965fd849a95196f2a2fb136033c
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-04-16 21:51:57 -0400

    add pages

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./Components/NavBar";
 
 // PAGES
-import Home from "./Pages/Home";
-import Index from "./Pages/Index";
-import Show from "./Pages/Show";
+import Home from "./pages/Home";
+import Index from "./pages/Index";
+import New from "./pages/New";
+import Show from "./pages/Show";
+import Edit from "./pages/Edit";
+import FourOFour from "./pages/FourOFour";
 
 function App() {
   return (
@@ -19,7 +22,10 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/logs" element={<Index />} />
-            {/* <Route path="/logs/:index" element={<Show />} /> */}
+            <Route path="/logs/new" element={<New />} />
+            <Route path="/logs/:index" element={<Show />} />
+            <Route path="/logs/:index/edit" element={<Edit />} />
+            <Route path="*" element={<FourOFour />} />
           </Routes>
         </main>
       </BrowserRouter>
