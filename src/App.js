ca74ce2efa9d320074f05ada0bfac164e2390fc3
HaroldF415 <harold.f415@gmail.com>
2023-04-17 09:18:05 -0400

    importing UpdateLog and Error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { NavBar, Home, LogsIndex, ShowLog, NewLog } from "./components";
+import { NavBar, Home, LogsIndex, ShowLog, NewLog, UpdateLog, Error } from "./components";
 
 const App = () => {
   return (
@@ -11,6 +11,8 @@ const App = () => {
           <Route path="/logs" element={<LogsIndex />} />
           <Route path="/logs/new" element={<NewLog />} />
           <Route path="/logs/:index" element={<ShowLog />} />
+          <Route path="/logs/:index/edit" element={<UpdateLog />} />
+          <Route path="*" element={<Error />} />
         </Routes>
       </Router>
     </div>
--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,7 @@ import Home from "./Home/Home";
 import LogsIndex from "./Log/LogsIndex";
 import ShowLog from "./Log/ShowLog";
 import NewLog from "./Log/NewLog";
+import UpdateLog from "./Log/UpdateLog";
+import Error from "./common/Error/Error";
 
 export { NavBar, Home, LogsIndex, ShowLog, NewLog };
