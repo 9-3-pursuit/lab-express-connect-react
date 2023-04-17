a9e5f906849ee85ceb65879aa225fa4cd9e605a3
HaroldF415 <harold.f415@gmail.com>
2023-04-17 03:41:35 -0400

    logsIndex file added to barrel exporting

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 import NavBar from "./common/NavBar/NavBar";
 import Home from "./Home/Home";
-export { NavBar, Home };
+import LogsIndex from "./Log/LogsIndex";
+
+export { NavBar, Home, LogsIndex };
